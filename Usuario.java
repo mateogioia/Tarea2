77996f822e18dabe8eb1f67dea616c85960b4995
mateogioia <62824970+mateogioia@users.noreply.github.com>
2020-04-05 18:57:53 -0300

    Getters, setters y constructor

--- Usuario.java
+++ Usuario.java
@@ -7,4 +7,60 @@ public class Usuario {
     private int cantHijos;
     private int saldo;
     
+    public Usuario() {
+		this.nombre = "";
+		this.contraseña = "";
+		this.edad = 0;
+        this.cedula = 0;
+        this.cantHijos = 0;
+		this.saldo = 0;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String unNombre) {
+        this.nombre = unNombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int unaEdad) {
+        this.edad = unaEdad;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String unaContraseña) {
+        this.contraseña = unaContraseña;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int unaCedula) {
+        this.cedula = unaCedula;
+    }
+
+    public int getCantHijos() {
+        return cantHijos;
+    }
+
+    public void setCantHijos(int hijos) {
+        this.cantHijos = hijos;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int unSaldo) {
+        this.saldo = unSaldo;
+    }
+    
 }
