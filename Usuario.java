345d8327a804b64a857487a9da5a802e9b720eb6
mateogioia <62824970+mateogioia@users.noreply.github.com>
2020-04-05 18:51:53 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+    
+    private String nombre;
+    private int edad;
+    private String contraseña;
+    private int cedula;
+    private int cantHijos;
+    private int saldo;
+    
+}
